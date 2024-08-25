226a433e219e8269b7447fb054ba193fa6dd4a66
ianramy <imwagore@gmail.com>
2024-08-25 20:12:45 +0300

    origin

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,24 @@
 
 def happy_new_year():
     # code goes here!
+    for num in range(10,0,-1):
+        print(num)
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [x ** 2 for x in int_list]
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
